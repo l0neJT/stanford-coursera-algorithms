cacadb375b773d4e670960a45c498d13a0979278
Logan J Travis <loganjtravis@gmail.com>
2017-07-10 01:20:32 +0000

    crs02-wk01: Re-run for full dataset; Node count LOW

--- crs02/wk01/prog-assign/Assignment.py
+++ crs02/wk01/prog-assign/Assignment.py
@@ -99,7 +99,7 @@ nodesBak = defaultdict(list)
 edges = []
 
 # Open file and split into lines
-f = open('SCC_StudentTest01.txt', 'r')
+f = open('SCC.txt', 'r')
 
 # Populate nodes and back pointer dictionaries
 for line in list(f):
@@ -117,9 +117,9 @@ for line in list(f):
     nodesBak[e[1]].append(i)
     edges.append(e)
 
-print nodes
-print nodesBak
-print edges[:10]
+print 'Count nodes:', len(nodes)
+print 'Count nodes in back pointer dict:', len(nodesBak)
+print 'Count edges:', len(edges)
 
 #
 # Reverse DFS to get finish times
@@ -128,10 +128,10 @@ print edges[:10]
 finishTime = []
 dfsIter(nodesBak, edges, range(len(nodes), 0, -1), finishTime)
 
-print finishTime[:100]
+print 'Finish time list length:', len(finishTime)
 
 #
 # Normal DFS to get strongly connected components
 #
 scc = dfsIter(nodes, edges, list(reversed(finishTime)))
-json.dump(scc, open('SCC_StudentTest01.json', 'w'))
+json.dump(scc, open('SCC.json', 'w'))
