d794e7d322c19c2ea8ba51c7f5f3ef820a61c287
Logan J Travis <loganjtravis@gmail.com>
2017-08-05 17:46:40 +0000

    crs03-wk01: Improve documentation for DifferenceJob class.

--- crs03/wk01/prog-assign/Assignment.py
+++ crs03/wk01/prog-assign/Assignment.py
@@ -12,45 +12,87 @@ from functools import total_ordering
 
 @total_ordering
 class DifferenceJob:
-    """
+    """A single job for use in a difference schedule.
     
+    Intended only to achieve the outcomes required for the programming
+    assignment in week one (1) of course three (3) of the Stanford Algorithms
+    Specialization offered through Coursera.
+
     """
     def __init__(self, weight = 1, runtime = 1):
-        """Instanciate a difference job with weight and runtime
+        """Instantiate a difference job with weight and runtime.Instantiate
         
+        Args:
+            weight (int): Value for completing the job. Defaults to one (1).
+            runtime (int): Time required to complete the job once started.
+                Defaults to one (1).
         """
-        self._weight, self._runtime = weight, runtime
-        self._difference = weight - runtime
+        self.__weight, self.__runtime = weight, runtime
+    
+    @property
+    def weight(self):
+        """int: Value for completing the job."""
+        return self.__weight
+    
+    @property
+    def runtime(self):
+        """int: Time required to complete the job once started."""
+        return self.__runtime
+    
+    @property
+    def difference(self):
+        """int: Calculated by subtracting runtime from weight."""
+        return self.weight - self.runtime
     
     def __eq__(self, other):
-        """Return true if two difference jobs have equal weight and runtime
+        """Return equality comparison between this difference job and another.
         
+        Args:
+            other(:obj:DifferenceJob): The comparison difference job.
+        
+        Returns:
+            bool: True if this difference job and the other have equal weight
+                and runtime properties. False otherwise.
         """
-        return self._weight == other._weight and self._runtime == other._runtime
+        return self.weight == other.weight and self.runtime == other.runtime
     
     def __lt__(self, other):
-        """Return true if this difference job has a lower difference than 
-        another difference job. Ties broken by the lesser weight.another
+        """Return less than comparison between this diference job and another.
         
+        Args:
+            other(:obj:DifferenceJob): The comparison difference job.
+            
+        Returns:
+            bool: True if this difference job has a strictly lower difference
+                (weight - runtime) than the other difference job or if they have
+                equal differences but this job has a strictly lower weight.
+                False otherwise.
         """
         return (
-            self._difference < other._difference or (
-                self._difference == other._difference
-                and self._weight < other._weight
+            self.difference < other.difference or (
+                self.difference == other.difference
+                and self.weight < other.weight
             )
         )
         
     def __str__(self):
-        """Return a string representation
+        """Return a string representation.
         
+            Returns:
+                str: A string in the format (weight, runtime)
         """
-        return "{\"_difference\":" + str(self._difference) + ",\"_weight\":" + str(self._weight) + ",\"_runtime\":" + str(self._runtime) + "}"
+        return "(" + str(self.__weight) + "," +  str(self.__runtime) + ")"
         
     def __repr__(self):
-        """Return string representation
+        """Return a string representation.
         
+            Identical to str(self).
+        
+            Returns:
+                str: A string in the format (weight, runtime)
         """
         return str(self)
+
 #
 #   Main
 #
