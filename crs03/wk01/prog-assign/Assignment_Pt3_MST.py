f39add1f347ed5fb14f0d23d4f35f25297f478dd
Logan J Travis <loganjtravis@gmail.com>
2017-08-05 19:57:34 +0000

    crs03-wk01: Create stub assignment for part 3 and upload edges dataset.

---
+++ crs03/wk01/prog-assign/Assignment_Pt3_MST.py
@@ -0,0 +1,7 @@
+#   Stanford Algorithms Specialization on Coursera
+#   Course 03
+#   Week 01 - Programming Assignment - Part 3: Minimum Spanning Tree
+#
+#   2017-08-05
+#   Logan J Travis
+#   loganjtravis@gmail.com
