692479e411d217699f5fdcd0c83e0803c71305eb
Logan J Travis <loganjtravis@gmail.com>
2017-08-19 20:31:37 +0000

    crs03-wk03: Initialize part 1 of programming assignment and upload test data.

---
+++ crs03/wk03/prog-assign/Assignment_Pt1_Huffman.py
@@ -0,0 +1,7 @@
+#   Stanford Algorithms Specialization on Coursera
+#   Course 03
+#   Week 03 - Programming Assignment - Part 1: Huffman Coding
+#
+#   2017-08-19
+#   Logan J Travis
+#   loganjtravis@gmail.com
