0372484b856642392e2fb5175df292c0c3cbe552
Logan J Travis <loganjtravis@gmail.com>
2017-09-17 23:41:44 +0000

    crs04-wk02: It worksgit add .! Just a bit of cleanup.

--- crs04/wk02/prog-assign/Assignment-ScatterPlot.py
+++ crs04/wk02/prog-assign/Assignment-ScatterPlot.py
@@ -14,48 +14,21 @@ import matplotlib.pyplot as plt
 #
 # Main
 #
-# # x and y-axes
-# x = []
-# y = []
+# x and y-axes
+x = []
+y = []
 
-# # Read nodes from file with euclidian x and y-coordinates
-# with open('tsp.txt', 'r') as f:
-#     # Skip first line
-#     next(f)
+# Read nodes from file with euclidian x and y-coordinates
+with open('tsp.txt', 'r') as f:
+    # Skip first line
+    next(f)
     
-#     # Create and add edge from each line
-#     for l in f:
-#         n = l.strip().split(" ")
-#         x.append(n[0])
-#         y.append(n[1])
+    # Create and add edge from each line
+    for l in f:
+        n = l.strip().split(" ")
+        x.append(n[0])
+        y.append(n[1])
 
-# # print x, y
-# plt.scatter(x, y)
-# plt.savefig("tsp-ScatterPlot.svg")
-
-# # x and y-axes
-# nodes = []
-# edges = []
-
-# # Read nodes from file with euclidian x and y-coordinates
-# with open('tsp.txt', 'r') as f:
-#     # Skip first line
-#     next(f)
-    
-#     # Create and add edge from each line
-#     for l in f:
-#         node = map(lambda v: float(v), l.strip().split(" "))
-#         iNode = len(nodes)
-        
-#         # Create edges to all preceding nodes
-#         for i, n in enumerate(nodes):
-#             edges.append([i+1, iNode+1, ((n[0] - node[0])**2 + (n[1] - node[1])**2)**(0.5)])
-        
-#         # Add node
-#         nodes.append(node)
-
-# print edges
-
-from itertools import combinations
-for d in xrange(1,5):
-    print list(combinations(range(2,6), d))
+# print x, y
+plt.scatter(x, y)
+plt.savefig("tsp-ScatterPlot.svg")
--- crs04/wk02/prog-assign/Assignment.py
+++ crs04/wk02/prog-assign/Assignment.py
@@ -9,6 +9,7 @@
 # Imports
 from collections import defaultdict
 from datetime import datetime
+from itertools import combinations
 
 class tspGraph:
     """Basic graph class with traveling salesman methods.
@@ -24,56 +25,58 @@ class tspGraph:
     MaxNodeCount = 31
     """int: Maximum number of supported nodes."""
     
-    def __init__(self, edges = None):
+    def __init__(self, nodes = None):
         """Initalize a new graph with optional list of edges.
         
         Args:
-            edges(:obj:`list` of :obj:`list`, optional): List of edges in format
-                [head, tail, weight]. Weight is optional.
+            nodes(:obj:`list` of :obj:`list`, optional): List of nodes with
+                format (name, x, y).
         """
-        self.__nodes = defaultdict(lambda: {"tails":[], "heads":[]})
-        self.__edges = []
-        if edges is not None: self.addEdgeList(edges)
+        self.__nodes = []
+        self.__nodesRev = {}
+        self.__nodeDist = [[]]
+        if nodes is not None: self.addNodeList(nodes)
 
     @property
     def nodeCount(self):
         """int: Number of nodes"""
         return len(self.__nodes)
     
-    @property
-    def edgeCount(self):
-        """int: Number of edges"""
-        return len(self.__edges)
-    
-    def addEdge(self, tail, head, weight = None):
-        """Add a single edge to the graph. Creates necessary nodes if missing.
+    def addNode(self, name, x, y):
+        """Add a single node to the graph.
         
         Args:
-            tail (:hashable:): Tail node of the new edge.
-            head (:hashable:): Head node of the new edge.
-            weight (int, optional): Weight of the edge. Defaults to class
+            name (:hashable:): Node name.
+            x (float): X position of new node.
+            y (float): Y position of new node.
                 property DefaultWeight.
         """
-        weight = self.DefaultWeight if weight is None else weight
-        
-        # Add edge
-        idx = self.edgeCount
-        self.__edges.append((tail, head, weight))
-        
-        # Add edge reference to nodes
-        self.__nodes[head]["heads"].append(idx)
-        self.__nodes[tail]["tails"].append(idx)
+        i = self.nodeCount
+        self.__nodes.append((float(x), float(y)))
+        self.__nodesRev[name] = i
 
-    def addEdgeList(self, edges):
-        """Add multiple edges from a list. Creates necessary nodes if missing.
+    def addNodeList(self, nodes):
+        """Add multiple nodes from a list.
         
         Args:
-            edges (:obj:`list` of :obj:`list`): List of edges in format [head,
-                tail, weight]. Weight is optional.
+            nodes(:obj:`list` of :obj:`list`): List of nodes with format
+                (name, x, y).
         """
-        for e in edges:
-            self.addEdge(e[0], e[1], e[2] if len(e) > 2 else None)
-
+        for n in nodes:
+            self.addNode(n[0], n[1], n[2])
+            
+    def calcNodeDists(self):
+        """Calcuate distances between all nodes and save for later lookup.""" 
+        self.__nodeDist = [[0 for x in range(self.nodeCount)]\
+            for y in range(self.nodeCount)\
+        ] 
+        for i in xrange(0, self.nodeCount):
+            for j in xrange(0, self.nodeCount):
+                self.__nodeDist[i][j] = 0 if i == j else (
+                    (self.__nodes[i][0] - self.__nodes[j][0])**2 + \
+                    (self.__nodes[i][1] - self.__nodes[j][1])**2
+                )**(0.5)
+            
     def minTSP(self):
         """Return the shortest path length for traveling salesman.
         
@@ -87,59 +90,65 @@ class tspGraph:
                 (self.nodeCount, self.MaxNodeCount)\
             )
         
-        # Create node bitmasks and notter (for getting edges not in a set)
-        masks = dict(map(lambda (i, k): (k, 2**i), \
-            enumerate(self.__nodes.keys())\
-        ))
-        notter = 2**(self.nodeCount) - 1
-        
-        # Initialize working dictionaries
-        curr = {1:{masks[1]:0}}
-        prev = {}
+        # Calculate all node distances
+        self.calcNodeDists()
+
+        # Initialize working dictionary
+        curr = defaultdict(lambda: [self.Inf] * self.nodeCount)
+
+        # Add single edge paths
+        for i in xrange(1, self.nodeCount):
+            curr[1 + i**2][i] = self.__nodeDist[0][i]
         
         # Iterate through maximum number of edges in valid paths
-        for i in xrange(1, self.nodeCount + 1):
+        for i in xrange(2, self.nodeCount):
+            # Swap current working dictionary to previous
+            prev = curr
+            curr = defaultdict(lambda: [self.Inf] * self.nodeCount)
+            
             # Print iteration
             print "Iteration", i, "started at time", datetime.now().time()
+
+            # Generate sets (excluding zero)
+            sets = combinations(range(1, self.nodeCount), i)
             
-            # Swap working dictionaries
-            prev = curr
-            curr = defaultdict(lambda: defaultdict(lambda: self.Inf))
-            
-            # Iterate through previous node sets
-            for s in prev.keys():
-                # Get nodes not in set; NOTE: final iteration of outer for loop
-                # returns to starting node
-                notS = 1 if i == self.nodeCount else s ^ notter
+            # Iterate through each set
+            for s in sets:
+                # Calculate the set mask
+                sMask = 1 + sum(map(lambda i: 2**i , s))
                 
-                # Iterate through last node in set
-                for k in prev[s].keys():
-                    # Get all incident edges
-                    edges = self.__nodes[k]["heads"]
-                    edges.extend(self.__nodes[k]["tails"])
+                # Iterate through each node in the set
+                for j in s:
+                    # Calcualte the set mask without this node
+                    sMask_j = sMask - 2**j
                     
-                    # Iterate through all edges
-                    for e in set(edges):
-                        edge = self.__edges[e]
-                        j = edge[1] if edge[0] == k else edge[0]
-                        ln = prev[s][k] + edge[2] * 1.0
+                    # Iterate through the other nodes
+                    for k in s:
+                        if k == j: continue
                         
-                        # Add path to current node if not in set and has minimum
-                        # length for current paths
-                        if notS & masks[j] == masks[j] and \
-                            curr[s | masks[j]][j] > ln\
-                        :
-                            curr[s | masks[j]][j] = ln
+                        # Calcuate new, full path length
+                        ln = prev[sMask_j][k] + self.__nodeDist[k][j]
+                        
+                        # Set path length to minimum
+                        curr[sMask][j] = min(ln, curr[sMask][j])
+            
+        # Calculate minimum return path
+        fullMask = 2**self.nodeCount - 1
+        for k in xrange(1, self.nodeCount):
+            # Calcuate new, full path length
+            ln = curr[fullMask][k] + self.__nodeDist[k][0]
+            
+            # Set path length to minimum
+            curr[fullMask][0] = min(ln, curr[fullMask][0])
         
         # Return shortest path length
-        return curr[notter][1]
+        return curr[fullMask][0]
 
 #
 # Main
 #
-# Create temporary arrays for nodes and edges
-nodes = []
-edges = []
+# Instantiate graph
+g = tspGraph()
 
 # Read nodes from file with euclidian x and y-coordinates
 with open('tsp.txt', 'r') as f:
@@ -147,24 +156,12 @@ with open('tsp.txt', 'r') as f:
     next(f)
     
     # Create and add node from each line
-    for l in f:
-        node = map(lambda v: float(v), l.strip().split(" "))
-        iNode = len(nodes)
-        
-        # Create edges to all preceding nodes
-        for i, n in enumerate(nodes):
-            edges.append([i+1, iNode+1, \
-                ((n[0] - node[0])**2 + (n[1] - node[1])**2)**(0.5)\
-            ])
-        
-        # Add node
-        nodes.append(node)
-
-# Instantiate graph
-g = tspGraph(edges)
+    for (i, l) in enumerate(f):
+        n = map(lambda v: float(v), l.strip().split(" "))
+        g.addNode(i+1, n[0], n[1])
 
 # Print graph summary
-print "Graph with %d nodes and %d edges." % (g.nodeCount, g.edgeCount)
+print "Graph with %d nodes." % (g.nodeCount)
 
 # Print shortest traveling salesman path
 print "Shortest traveling salesman path has length %.2f." % (g.minTSP())
